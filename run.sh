aa950953e369e2ba2e40f8b0a5a00dc4ed43cb6a
luanaruiz9 <33042466+luanaruiz9@users.noreply.github.com>
2023-05-09 08:57:51 -0400

    New batch of sims

--- run.sh
+++ run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-python main_all.py 6 0.01 32
-python main_all.py 6 0.01 'all'
-python main_all.py 6 0.1 32
-python main_all.py 6 0.1 'all'
-python main_all.py 6 0 32
-python main_all.py 6 0 'all'
+python main_all.py 10 0 32
 python main_all.py 10 0.01 32
-python main_all.py 10 0.01 'all'
+python main_all.py 10 0.1 32

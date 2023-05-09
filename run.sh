651eec2cb70f004eefed8de78aa61c2849869e24
luanaruiz9 <33042466+luanaruiz9@users.noreply.github.com>
2023-05-09 09:58:17 -0400

    Final version

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 python main_all.py 10 0 32
-python main_all.py 10 0.01 32
 python main_all.py 10 0.1 32
+python main_all.py 6 0 32
+python main_all.py 6 0.01 32
+python main_all.py 6 0.1 32
+python main_all_label.py 10 0 32
+python main_all_label.py 10 0.1 32
+python main_all_label.py 6 0 32
+python main_all_label.py 6 0.01 32
+python main_all_label.py 6 0.1 32
